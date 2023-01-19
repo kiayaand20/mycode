8d4c982363bb6b325d6959f0751be6a8277175cb
kiayaand20 <kiayaanderson89@gmail.com>
2023-01-19 20:40:28 +0000

    updated trivia game

---
+++ challenges/challenge103_106.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+
+from flask import Flask
+from flask import redirect
+from flask import request
+from flask import render_template
+
---
+++ challenges/trivia.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Friday Warmup | Returning Data From Complex JSON"""
+
+import requests
+
+URL= "https://opentdb.com/api.php?amount=3&category=12&difficulty=easy&type=multiple"
+
+def main():
+    #input from user to start trivia game
+    game_start = input("Would you like to play trivia game?(Y/N): ")
+
+    # data will be a python dictionary rendered from your API link's JSON!
+    if game_start.lower() == "y":
+        data = requests.get(URL).json()
+        print("Question 1:", data["results"][0]["question"])
+        print(data["results"][0]["incorrect_answers"])
+        print("\nCorrect Answer:")
+        print(data["results"][0]["correct_answer"])
+        print("\nNext question - \n")
+
+        print("Question 2:", data["results"][1]["question"])
+        print(data["results"][1]["incorrect_answers"])
+        print("\nCorrect Answer:")
+        print(data["results"][1]["correct_answer"])
+        print("\nLast question - \n")
+
+        print("Question 3:", data["results"][2]["question"])
+        print(data["results"][2]["incorrect_answers"])
+        print("\nCorrect Answer:")
+        print(data["results"][2]["correct_answer"])
+        print("\nThanks for playing!!!")
+    elif game_start.lower() == "n":
+        print("See you next time!")
+
+if __name__ == "__main__":
+    main()

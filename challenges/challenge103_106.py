dab69081ce7786221b9ca5b9ff50e4ee1571e720
kiayaand20 <kiayaanderson89@gmail.com>
2023-01-20 01:27:29 +0000

    working on challenge 103 & 106

--- challenges/challenge103_106.py
+++ challenges/challenge103_106.py
@@ -5,3 +5,22 @@ from flask import redirect
 from flask import request
 from flask import render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("practice.html")
+
+@app.route("/correct")
+def success():
+    return f"You are correct! Great job!"
+
+@app.route("/login", methods= ["POST"])
+def login():
+    if request.form.get("nm") and request.form.get("nm") == "Ruby":
+        return redirect("/correct")
+    else:
+        return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
--- challenges/trivia.py
+++ challenges/trivia.py
@@ -2,6 +2,7 @@
 """Friday Warmup | Returning Data From Complex JSON"""
 
 import requests
+import html
 
 URL= "https://opentdb.com/api.php?amount=3&category=12&difficulty=easy&type=multiple"
 
@@ -12,19 +13,19 @@ def main():
     # data will be a python dictionary rendered from your API link's JSON!
     if game_start.lower() == "y":
         data = requests.get(URL).json()
-        print("Question 1:", data["results"][0]["question"])
+        print("Question 1:", html.unescape(data["results"][0]["question"]))
         print(data["results"][0]["incorrect_answers"])
         print("\nCorrect Answer:")
         print(data["results"][0]["correct_answer"])
         print("\nNext question - \n")
 
-        print("Question 2:", data["results"][1]["question"])
+        print("Question 2:", html.unescape(data["results"][1]["question"]))
         print(data["results"][1]["incorrect_answers"])
         print("\nCorrect Answer:")
         print(data["results"][1]["correct_answer"])
         print("\nLast question - \n")
 
-        print("Question 3:", data["results"][2]["question"])
+        print("Question 3:", html.unescape(data["results"][2]["question"]))
         print(data["results"][2]["incorrect_answers"])
         print("\nCorrect Answer:")
         print(data["results"][2]["correct_answer"])
